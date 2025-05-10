ab2e40b1069b040515055118512cac2d824c3813
adamwahyuh <adamwahyu875@gmail.com>
2025-05-10 17:32:23 +0700

    test database connection

--- backend/koneksi.php
+++ backend/koneksi.php
@@ -2,8 +2,10 @@
 
 // aku mau lambo
 try{
-    $kon = new \PDO('sqlite:' . __DIR__ . '/../database/database.db');
+    $kon = new \PDO('sqlite: .'.'/../database/database.db');
     echo "Duck is Ducking in the River";
 }catch(\PDOException $e){
     echo $e->getMessage();
 }
+
+include"worker.php";
